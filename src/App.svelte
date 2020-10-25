fcb974f4f14815e90c7600be043486ef5d791eda
Leroy Campbell <leroy@artisonian.com>
2020-10-25 09:36:45 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Tailwind from "./Tailwind.svelte";
+
+  export let name;
+</script>
+
+<Tailwind />
+
+<main class="max-w-sm mx-auto p-4 text-center">
+  <h1 class="text-6xl text-red-500 font-hairline uppercase">Hello {name}!</h1>
+  <p class="mt-4">
+    Visit the
+    <a
+      class="text-blue-500 hover:underline"
+      href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
